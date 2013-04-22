45e81675146d3961a96b77f078affb9f8a4f0e07
Freddie Fujiwara <github@ze.gs>
2013-04-22 12:47:46 +0900

    license

--- src/HTML/ExtractContent.php
+++ src/HTML/ExtractContent.php
@@ -1,4 +1,13 @@
 <?php
+/** 
+ * HTML_ExtractContent extract the text from web page(html). 
+ * fork from "Extract Content Module for html" 
+ * @link http://www.systemfriend.co.jp/node/326 
+ * @link http://hakaselab.sakura.ne.jp/make/extractcontent/extractcontent.phps 
+ * @link http://openpear.org/package/HTML_ExtractContent
+ * PHP-version author is Junichi Takahashi 
+ * @license BSD 
+ * */
 namespace HTML;
 
 class ExtractContent
